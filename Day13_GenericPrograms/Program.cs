6ca40e56f635de07e6fd1646bd24d8ca4dabf753
unknown <neeladri22@gmail.com>
2022-06-26 22:40:40 +0530

    init commit

---
+++ Day13_GenericPrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_GenericPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three Integer Value");
+            int firstValue = int.Parse(Console.ReadLine());
+            int secondValue = int.Parse(Console.ReadLine());
+            int thirdValue = int.Parse(Console.ReadLine());
+            UC1_MaxIntgerNumbers.MaxInteger(firstValue, secondValue, thirdValue);
+        }
+    }
+}

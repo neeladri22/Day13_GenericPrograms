581c91e3e40b5d751250749c817cc128de53780e
unknown <neeladri22@gmail.com>
2022-06-27 10:32:15 +0530

    Added Refactor 2 program file

--- Day13_GenericPrograms/Program.cs
+++ Day13_GenericPrograms/Program.cs
@@ -10,16 +10,16 @@ namespace Day13_GenericPrograms
     {
         static void Main(string[] args)
         {
-            Refactor1<int> refactorint = new Refactor1<int>();
-            int max1 = refactorint.MaxValue(40, 50, 60);
+            Refactor2<int> refactorint = new Refactor2<int>(30, 40, 50);
+            int max1 = refactorint.MaxValueOf(30,40,50);
             Console.WriteLine("Maximum value of int is: " + max1);
 
-            Refactor1<double> refactorfloat = new Refactor1<double>();
-            double max2 = refactorfloat.MaxValue(10.5, 20.5, 30.5);
+            Refactor2<double> refactorfloat = new Refactor2<double>(10.5, 20.5, 30.5);
+            double max2 = refactorfloat.MaxValueOf(10.5, 20.5, 30.5);
             Console.WriteLine("Maximum value of float is: " + max2);
 
-            Refactor1<string> refactorstring = new Refactor1<string>();
-            string max3 = refactorstring.MaxValue("neelu", "varsh", "ashok");
+            Refactor2<string> refactorstring = new Refactor2<string>("Abhi", "avi", "lol");
+            string max3 = refactorstring.MaxValueOf("neelu", "varsha", "ashok");
             Console.WriteLine("Maximum value of string is: " + max3);
         }
     }
---
+++ Day13_GenericPrograms/Refactor2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_GenericPrograms
+{
+    internal class Refactor2<T> where T : IComparable
+    {
+        public T first, second, third;
+
+        public Refactor2(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public T MaxValueOf(T first, T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("First Value is maximum");
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("Second Value is Maximum ");
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0 ||
+                    third.CompareTo(second) >= 0 && third.CompareTo(first) > 0 ||
+                    third.CompareTo(second) > 0 && third.CompareTo(first) >= 0)
+            {
+                Console.WriteLine("Third Value is maximum");
+                return third;
+            }
+            if (first.CompareTo(second) == 0 && first.CompareTo(third) == 0 && second.CompareTo(third) == 0)
+            {
+                Console.WriteLine("All three Values are same");
+            }
+            return default;
+        }
+    }
+}
